6ba3821db2872b5f02b99328efe025c682bc970a
kohei.okazaki <kou1210hei@gmail.com>
2020-06-22 00:35:06 +0900

    ・プロジェクト大枠を作成 ・管理サイト側へのテーブルを登録 ・テーブルを用意 ・Bootstrap用意 ・DBはSQLiteからMySQLを使用するように変更

---
+++ philip/sns/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from sns.models import Message, Group, Friend, Good
+
+# Register your models here.
+admin.site.register(Message)
+admin.site.register(Group)
+admin.site.register(Friend)
+admin.site.register(Good)
---
+++ philip/sns/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Messageクラス
+class Message(models.Model):
+
+    # 投稿者情報
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_owner')
+    # グループ情報
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    # 投稿内容
+    content = models.TextField(max_length=200)
+    # share時のID
+    share_id = models.IntegerField(default=-1)
+    # いいね数
+    good_count = models.IntegerField(default=0)
+    # share数
+    share_count = models.IntegerField(default=0)
+    # 投稿日時
+    pub_date = models.DateTimeField(auto_now_add=True)
+    # 登録日時
+    reg_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.content) + ' (' + str(self.owner) + ')'
+
+    def get_share(self):
+        return Message.objects.get(id=self.share_id)
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+
+# Groupクラス
+class Group(models.Model):
+
+    # 投稿者情報
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='group_owner')
+    # グループタイトル
+    title = models.CharField(max_length=100)
+    # 登録日時
+    reg_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title)
+
+
+# Friendクラス
+class Friend(models.Model):
+
+    # 投稿者情報
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_owner')
+    # ユーザ情報
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # グループ情報
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    # 登録日時
+    reg_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user) + ' (group:"' + str(self.group) + '")'
+
+
+# Goodクラス
+class Good(models.Model):
+
+    # 投稿者情報
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='good_owner')
+    # メッセージ情報
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    # 登録日時
+    reg_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'good for "' + str(self.message) + '" (by ' + str(self.owner) + ')'
+
